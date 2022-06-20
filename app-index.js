1bcb8e00b3140d7313e7113b546c64d83eb9039b
gianbarbaro <gianlbarbaro@gmail.com>
2022-06-19 21:01:13 -0300

    arreglo detalles en tabla de cotizacion

--- app-index.js
+++ app-index.js
@@ -1,36 +1,70 @@
 const coti = document.querySelector(".cotizacion");
 
+const nombre = document.getElementById("nombre");
+const compra = document.getElementById("compra");
+const venta = document.getElementById("venta");
+const variacion = document.getElementById("variacion");
+const fecha = document.getElementById("fecha");
 
-fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
-    .then(res => res.json())
-    .then(data => {
-        for (const ele of data) {
-            // console.log(ele.casa.nombre) //primer logro.
-            if (ele.casa.nombre !== 'Argentina' && ele.casa.nombre !== 'Dolar Soja') {
-                // console.log(ele.casa.nombre);
-                coti.innerHTML += `<tr>
-                <td>${ele.casa.nombre}</td>
-                <td>${ele.casa.compra}</td>
-                <td>${ele.casa.venta}</td>
-                <td>${variacion(ele.casa.variacion)}</td>
-                <td>${fecha_Actu()}</td>
-                </tr>`;
-            }
-        }
-    })
-    
-function variacion(varia) {
-    if (varia == undefined) {
-        return '-';
-    } else {
-        return varia;
+fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
+  .then((res) => res.json())
+  .then((data) => {
+    for (const ele of data) {
+      // console.log(ele.casa.nombre) //primer logro.
+      if (ele.casa.nombre !== "Argentina" && ele.casa.nombre !== "Dolar Soja") {
+        // console.log(ele.casa.nombre);
+        imprimirDatos(ele)
+        
+      }
     }
-}
-function fecha_Actu(){
-    const date = new Date();
-    const dia = date.getDate();
-    const mes = date.getMonth();
-    const año = date.getFullYear();
+  });
 
-    return dia+"/"+mes+"/"+año;
+function imprimirDatos(elemento) {
+
+    p1 = document.createElement("p")
+    p1.innerHTML = elemento.casa.nombre;
+    
+    p2 = document.createElement("p")
+    p2.innerHTML = verificacion(elemento.casa.compra);
+    
+    p3 = document.createElement("p")
+    p3.innerHTML = "$"+elemento.casa.venta;
+    
+    p4 = document.createElement("p");
+    p4.innerHTML = variaSiONo(elemento.casa.variacion);
+    p4.className = elemento.casa.variacion.includes("-") ? p4.className = "varia-red" : p4.className = "varia-green";
+    
+    p5 = document.createElement("p")
+    p5.innerHTML = fecha_Actu();
+    
+    nombre.appendChild(p1)
+    compra.appendChild(p2)
+    venta.appendChild(p3)
+    variacion.appendChild(p4)
+    fecha.appendChild(p5)
+}
+
+function variaSiONo(varia) {
+  if (varia == undefined) {
+    return "-";
+  } else {
+    return varia + "%";
+  }
+}
+
+function fecha_Actu() {
+  const date = new Date();
+  const dia = date.getDate();
+  const mes = date.getMonth()+1;
+  const año = date.getFullYear();
+
+  return dia + "/" + mes + "/" + año;
+}
+
+function verificacion(elemento) {
+    if(elemento === "No Cotiza") {
+        return elemento;
+    } else {
+        return "$"+elemento;
+    }
 }
